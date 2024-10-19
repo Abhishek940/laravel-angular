307c6cfa88ee76e912f2565dae6b7734120f0e95
CSMCORP\abhishek.k <abhishek.k@csm.tech>
2024-10-19 19:19:26 +0530

    changes

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -28,7 +28,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
    
 
